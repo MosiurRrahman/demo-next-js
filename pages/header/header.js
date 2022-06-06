00776919d473bc4daec6dcbddaeaeaa8030ccd39
MosiurRrahman <mosiurrahman595@gmail.com>
2022-06-06 16:15:46 +0600

    next demo project

---
+++ pages/header/header.js
@@ -0,0 +1,7 @@
+export default function header(){
+    return (
+        <h1 >
+          Welcome todadada <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from './header/header'
 
 export default function Home() {
   return (
@@ -12,8 +13,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 >
+          <Header/>
         </h1>
 
         <p className={styles.description}>
